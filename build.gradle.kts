a0da05c12b2bde7f0be027370ef557bd4cf3d181
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-10-10 21:11:34 -0500

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.7.20"
+}
+
+group = "us.teaminceptus.plasmaenchants"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+
+}
+
+allprojects {
+    group = "us.teaminceptus.plasmaenchants"
+    version = "1.0.0-SNAPSHOT"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name="PlasmaEnchants"
+
+include("api")
+include("plugin")
