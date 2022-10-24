5a274daff11a70c4129c9729f534c554efbb1462
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-10-23 20:50:45 -0500

    Update PlasmaConfig.kt

--- api/src/main/kotlin/us/teaminceptus/plasmaenchants/api/PlasmaConfig.kt
+++ api/src/main/kotlin/us/teaminceptus/plasmaenchants/api/PlasmaConfig.kt
@@ -1,7 +1,8 @@
-package us.teaminceptus.plasmaenchants.api;
+package us.teaminceptus.plasmaenchants.api
 
-import org.bukkit.plugin.Plugin;
-import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin
+import org.bukkit.Bukkit
+import java.io.File
 import java.util.logging.Logger
 
 /**
@@ -9,7 +10,7 @@ import java.util.logging.Logger
  */
 interface PlasmaConfig {
 
-    companion object Config {
+    companion object {
         private val p: Plugin? = if (getPlugin() == null) null else getPlugin()
 
         /**
@@ -27,8 +28,8 @@ interface PlasmaConfig {
          */
         @JvmStatic
         fun getLogger(): Logger? {
-            if (p == null) return null;
-            return p.getLogger();
+            if (p == null) return null
+            return p.logger
         }
 
         /**
@@ -48,23 +49,52 @@ interface PlasmaConfig {
          */
         @JvmStatic
         fun print(e: Throwable) {
-            val logger: Logger? = getLogger();
-            if (logger == null) return;
+            val logger: Logger = getLogger() ?: return
 
-            logger.severe(e.javaClass.getSimpleName());
-            logger.severe("--------------");
-            logger.severe(e.message);
-            for (stack: StackTraceElement in e.stackTrace) logger.severe(stack.toString());
+            logger.severe(e.javaClass.simpleName)
+            logger.severe("--------------")
+            logger.severe(e.message)
+            for (stack: StackTraceElement in e.stackTrace) logger.severe(stack.toString())
+        }
+
+        /**
+         * Fetches the Plugin's Data Folder.
+         * @return Data Folder
+         */
+        fun getDataFolder(): File? {
+            return p?.dataFolder
+        }
+
+        /**
+         * Fetches the Player Data Directory.
+         * @return Player Data Directory
+         */
+        @JvmStatic
+        fun getPlayerDirectory(): File? {
+            return getDataFolder()?.resolve("players")
         }
     }
 
-    
+    /**
+     * Fetches a message from the Language file.
+     * @param key The key to fetch
+     * @return The message
+     */
     fun get(key: String): String
 
+    /**
+     * Fetches a message from the Language file, with the plugin prefix in front.
+     * @param key The key to fetch
+     * @return The message
+     */
     fun getMessage(key: String): String {
         return get("plugin.prefix") + get("plugin.prefix")
     }
 
+    /**
+     * Fetches the language set in the configuration.
+     * @return Language Configured
+     */
     fun getLanguage(): String?
 
 }
