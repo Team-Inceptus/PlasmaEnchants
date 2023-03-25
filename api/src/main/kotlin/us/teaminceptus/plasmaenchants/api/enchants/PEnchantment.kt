2e69e1aaead7acc5c56f4460aa79c79d69909a78
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-03-24 22:29:13 -0500

    Update PEnchantment APIs

--- api/src/main/kotlin/us/teaminceptus/plasmaenchants/api/enchants/PEnchantment.kt
+++ api/src/main/kotlin/us/teaminceptus/plasmaenchants/api/enchants/PEnchantment.kt
@@ -2,14 +2,59 @@ package us.teaminceptus.plasmaenchants.api.enchants
 
 import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.jetbrains.annotations.NotNull
 import java.util.Objects
+import java.util.function.BiConsumer
 import java.util.function.Consumer
 import java.util.stream.Collectors
 
 /**
  * Represents a PlasmaEnchants Enchantment.
  */
-interface PEnchantment : Consumer<Player> {
+interface PEnchantment : BiConsumer<Player, Int> {
+
+    /**
+     * Fetches the name of this PEnchantment.
+     * @return Name of Enchantment
+     */
+    fun getName(): String
+
+    /**
+     * Fetches the description of this PEnchantment.
+     * @return Description of Enchantment
+     */
+    fun getDescription(): String
+
+    /**
+     * Whether this Enchantment will spawn in an Enchantment Table.
+     * @return true if in enchant table, false otherwise
+     */
+    fun isNatural(): Boolean
+
+    /**
+     * Rolls the chance for this Enchantment's level in an enchantment table.
+     * @param player Player to use
+     * @return result of the roll, 0 if not in enchanting table
+     */
+    fun rollTable(player: Player): Int
+
+    /**
+     * Fetches the maximum level of this PEnchantment.
+     * @return Maximum Level
+     */
+    fun getMaxLevel(): Int = 1
+
+    /**
+     * Fetches the Target Type of this PEnchantment.
+     * @return Target Type
+     */
+    fun getTarget(): Target
+
+    /**
+     * Fetches the Type of this PEnchantment.
+     * @return Type
+     */
+    fun getType(): Type
 
     /**
      * PEnchantment Types
@@ -33,6 +78,7 @@ interface PEnchantment : Consumer<Player> {
      * PEnchantment Material Application Targets
      */
     enum class Target {
+
         /**
          * Represents all Swords
          */
@@ -44,6 +90,7 @@ interface PEnchantment : Consumer<Player> {
             Material.DIAMOND_SWORD,
             Material.matchMaterial("NETHERITE_SWORD") ?: Material.AIR
         ),
+
         /**
          * Represents all Axes
          */
@@ -55,6 +102,7 @@ interface PEnchantment : Consumer<Player> {
             Material.DIAMOND_AXE,
             Material.matchMaterial("NETHERITE_AXE") ?: Material.AIR
         ),
+
         /**
          * Represents all Pickaxes
          */
@@ -67,12 +115,102 @@ interface PEnchantment : Consumer<Player> {
             Material.matchMaterial("NETHERITE_PICKAXE") ?: Material.AIR
         ),
 
-        // TODO: Add more Material Targets
+        /**
+         * Represents all Shovels
+         */
+        SHOVELS(
+            Material.WOODEN_SHOVEL,
+            Material.STONE_SHOVEL,
+            Material.IRON_SHOVEL,
+            Material.GOLDEN_SHOVEL,
+            Material.DIAMOND_SHOVEL,
+            Material.matchMaterial("NETHERITE_SHOVEL") ?: Material.AIR
+        ),
+
+        /**
+         * Represents all Hoes
+         */
+        HOES(
+            Material.WOODEN_HOE,
+            Material.STONE_HOE,
+            Material.IRON_HOE,
+            Material.GOLDEN_HOE,
+            Material.DIAMOND_HOE,
+            Material.matchMaterial("NETHERITE_HOE") ?: Material.AIR
+        ),
+
+        /**
+         * Represents all Melee Weapons (Swords and Axes)
+         */
+        MELEE_WEAPONS(SWORDS, AXES),
+
+        /**
+         * Represents all mining tools (Shovels, Pickaxes, Axes, and Hoes)
+         */
+        MINING_TOOLS(SHOVELS, PICKAXES, AXES, HOES),
+
+        /**
+         * Represents all tools (Swords, Axes, Pickaxes, Shovels, and Hoes)
+         */
+        TOOLS(SWORDS, AXES, PICKAXES, SHOVELS, HOES),
+
+        /**
+         * Represents all Helmets
+         */
+        HELMETS(
+            Material.LEATHER_HELMET,
+            Material.CHAINMAIL_HELMET,
+            Material.IRON_HELMET,
+            Material.GOLDEN_HELMET,
+            Material.DIAMOND_HELMET,
+            Material.matchMaterial("NETHERITE_HELMET") ?: Material.AIR
+        ),
+
+        /**
+         * Represents all Chestplates
+         */
+        CHESTPLATES(
+            Material.LEATHER_CHESTPLATE,
+            Material.CHAINMAIL_CHESTPLATE,
+            Material.IRON_CHESTPLATE,
+            Material.GOLDEN_CHESTPLATE,
+            Material.DIAMOND_CHESTPLATE,
+            Material.matchMaterial("NETHERITE_CHESTPLATE") ?: Material.AIR
+        ),
+
+        /**
+         * Represents all Leggings
+         */
+        LEGGINGS(
+            Material.LEATHER_LEGGINGS,
+            Material.CHAINMAIL_LEGGINGS,
+            Material.IRON_LEGGINGS,
+            Material.GOLDEN_LEGGINGS,
+            Material.DIAMOND_LEGGINGS,
+            Material.matchMaterial("NETHERITE_LEGGINGS") ?: Material.AIR
+        ),
+
+        /**
+         * Represents all Boots
+         */
+        BOOTS(
+            Material.LEATHER_BOOTS,
+            Material.CHAINMAIL_BOOTS,
+            Material.IRON_BOOTS,
+            Material.GOLDEN_BOOTS,
+            Material.DIAMOND_BOOTS,
+            Material.matchMaterial("NETHERITE_BOOTS") ?: Material.AIR
+        ),
+
+        /**
+         * Represents all Armor (Helmets, Chestplates, Leggings, and Boots)
+         */
+        ARMOR(HELMETS, CHESTPLATES, LEGGINGS, BOOTS),
 
         /**
          * Target that applies to all materials.
          */
-        ALL(SWORDS, AXES, PICKAXES)
+        ALL(TOOLS, ARMOR)
 
         ;
 
@@ -94,8 +232,7 @@ interface PEnchantment : Consumer<Player> {
                 .collect(Collectors.toList())
         }
 
-
-        fun mapTargets(targets: Array<Target>): ArrayList<Material> {
+        private fun mapTargets(targets: Array<Target>): ArrayList<Material> {
             val materials = ArrayList<Material>()
 
             targets.forEach { materials.addAll(it.getValidMaterials()) }
@@ -107,6 +244,7 @@ interface PEnchantment : Consumer<Player> {
          * Fetches all of the valid materiasl this Target supports.
          * @return Array of Valid Materials
          */
+        @NotNull
         fun getValidMaterials(): List<Material> {
             return validMaterials
         }
@@ -120,35 +258,5 @@ interface PEnchantment : Consumer<Player> {
             return validMaterials.contains(material)
         }
     }
-
-    /**
-     * Fetches the name of this PEnchantment.
-     * @return Name of Enchantment
-     */
-    fun getName(): String
-
-    /**
-     * Fetches the description of this PEnchantment.
-     * @return Description of Enchantment
-     */
-    fun getDescription(): String
-
-    /**
-     * Whether this Enchantment will spawn in an Enchantment Table.
-     * @return true if in enchant table, false otherwise
-     */
-    fun isNatural(): Boolean
-
-    /**
-     * Rolls the table chance for this Enchantment if it should spawn in an Enchantment Table.
-     * @return result of the roll
-     */
-    fun rollTableChance(): Boolean
-
-    /**
-     * Fetches the Target Type of this PEnchantment.
-     * @return Target Type
-     */
-    fun getTarget(): Target
 
 }
--- api/src/main/kotlin/us/teaminceptus/plasmaenchants/api/enchants/PEnchantments.kt
+++ api/src/main/kotlin/us/teaminceptus/plasmaenchants/api/enchants/PEnchantments.kt
@@ -1,17 +1,65 @@
 package us.teaminceptus.plasmaenchants.api.enchants
 
 import org.bukkit.entity.Player
-import java.util.function.Consumer
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+import us.teaminceptus.plasmaenchants.api.PlasmaConfig
+import us.teaminceptus.plasmaenchants.api.enchants.PEnchantment.Target.SWORDS
+import us.teaminceptus.plasmaenchants.api.enchants.PEnchantment.Target.MELEE_WEAPONS
+import us.teaminceptus.plasmaenchants.api.enchants.PEnchantment.Type.ATTACKING
 
 /**
  * Represents all of the Default PlasmaEnchants Enchantments.
  */
-enum class PEnchantments(private val action: Consumer<Player>) : PEnchantment {
+enum class PEnchantments(
+    private val nameKey: String,
+    private val type: PEnchantment.Type,
+    private val target: PEnchantment.Target,
+    private val maxLevel: Int = 1,
+    private val action: (Player, Int) -> Unit,
+    private val natural: Boolean = false,
+    private val naturalRoll: (Player) -> Int = { 0 },
+) : PEnchantment {
+
+    // Melee Enchantments
+
+    POISONING("enchantment.poisoning",
+        ATTACKING, SWORDS, 2, { p, level ->
+            p.addPotionEffect(PotionEffect(PotionEffectType.POISON, 20 * 5, level - 1))
+        }),
+
+    WITHERING("enchantment.withering",
+        ATTACKING, MELEE_WEAPONS, 1, { p, _ ->
+            p.addPotionEffect(PotionEffect(PotionEffectType.WITHER, 20 * 3, 0))
+        })
+
+    // Armor Enchantments
 
     ;
 
-    override fun accept(player: Player) {
-        action.accept(player)
+    constructor(
+        nameKey: String,
+        type: PEnchantment.Type,
+        target: PEnchantment.Target,
+        maxLevel: Int,
+        action: (Player, Int) -> Unit,
+        naturalRoll: (Player) -> Int,
+    ) : this(nameKey, type, target, maxLevel, action, true, naturalRoll)
+
+    override fun getName(): String = PlasmaConfig.getConfig()?.get(nameKey) ?: name.lowercase().replaceFirstChar { it.uppercase() }
+
+    override fun getDescription(): String = PlasmaConfig.getConfig()?.get("$nameKey.description") ?: "No description provided."
+
+    override fun isNatural(): Boolean = natural
+
+    override fun rollTable(player: Player): Int = naturalRoll.invoke(player)
+
+    override fun getType(): PEnchantment.Type = type
+
+    override fun getTarget(): PEnchantment.Target = target
+
+    override fun accept(player: Player, level: Int) {
+        action.invoke(player, level)
     }
 
 }
