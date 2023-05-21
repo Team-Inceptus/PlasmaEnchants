9a577db83d2d0922fecb1cee77752b18e91489d5
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-05-20 23:27:12 -0500

    Restrict Version to 1.14.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,11 +53,14 @@ allprojects {
     dependencies {
         compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.8.21")
         compileOnly("org.jetbrains:annotations:24.0.1")
-        compileOnly("org.spigotmc:spigot-api:1.14.4-R0.1-SNAPSHOT")
+        compileOnly("org.spigotmc:spigot-api:1.14.4-R0.1-SNAPSHOT") {
+            version {
+                strictly("1.14.4-R0.1-SNAPSHOT")
+            }
+        }
 
         testImplementation("org.mockito:mockito-core:5.3.1")
         testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
-        testImplementation("com.github.seeseemelk:MockBukkit-v1.13:0.2.0")
     }
 
     java {
