50380fdac51bf2e1c2e06eb4efc20f7d8d2277e0
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-06-12 01:45:04 -0500

    Simplify PlasmaEvents.kt

--- plugin/src/main/kotlin/us/teaminceptus/plasmaenchants/PlasmaEvents.kt
+++ plugin/src/main/kotlin/us/teaminceptus/plasmaenchants/PlasmaEvents.kt
@@ -14,7 +14,6 @@ import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 import us.teaminceptus.plasmaenchants.api.PType
-import us.teaminceptus.plasmaenchants.api.events.PlayerTickEvent
 import us.teaminceptus.plasmaenchants.api.getArtifact
 import us.teaminceptus.plasmaenchants.api.getPlasmaEnchants
 
@@ -24,65 +23,46 @@ internal class PlasmaEvents(plugin: PlasmaEnchants) : Listener {
         plugin.server.pluginManager.registerEvents(this, plugin)
     }
 
-    private fun execute(item: ItemStack, event: Event, type: PType<*>) {
+    private fun execute(player: Player?, event: Event, type: PType<*>) {
+        if (player == null) return
         if (event is Cancellable && event.isCancelled) return
-        val meta: ItemMeta = item.itemMeta ?: return
 
-        val enchants = meta.getPlasmaEnchants().filter { it.key.getType() == type }
-        enchants.forEach { it.key.accept(event, it.value) }
-
-        val artifact = meta.getArtifact().takeIf { it != null && it.getType() == type} ?: return
-        artifact.accept(event)
-    }
-
-    @EventHandler
-    fun onAttack(event: EntityDamageByEntityEvent) {
-        if (event.damager !is Player) return
-        val player = event.damager as Player
-
-        execute(player.inventory.itemInMainHand, event, PType.ATTACKING)
-    }
-
-    @EventHandler
-    fun onDefend(event: EntityDamageByEntityEvent) {
-        if (event.entity !is Player) return
-        val player = event.entity as Player
-        val items = player.inventory.armorContents
-
-        items.forEach { item -> execute(item, event, PType.DEFENDING) }
-    }
-
-    @EventHandler
-    fun onDamage(event: EntityDamageEvent) {
-        if (event.entity !is Player) return
-        val player = event.entity as Player
-        val items = player.inventory.armorContents
-
-        items.forEach { item -> execute(item, event, PType.DAMAGE) }
-    }
-
-    @EventHandler
-    fun onBlockBreak(event: BlockBreakEvent) = execute(event.player.inventory.itemInMainHand, event, PType.BLOCK_BREAK)
-
-    @EventHandler
-    fun onBlockDamage(event: BlockDamageEvent) = execute(event.player.inventory.itemInMainHand, event, PType.MINING)
-
-    @EventHandler
-    fun passive(event: PlayerTickEvent) {
-        val player = event.player
-        val items = listOfNotNull(
+        val items = listOf(
             player.inventory.armorContents.toList(),
             listOf(player.inventory.itemInMainHand),
             listOf(player.inventory.itemInOffHand)
         ).flatten()
 
-        items.forEach { item -> execute(item, event, PType.PASSIVE) }
+        items.forEach { item ->
+            val meta: ItemMeta = item.itemMeta ?: return
+
+            val enchants = meta.getPlasmaEnchants().filter { it.key.type == type }
+            enchants.forEach { it.key.accept(event, it.value) }
+
+            val artifact = meta.getArtifact().takeIf { it != null && it.type == type } ?: return
+            artifact.accept(event)
+        }
     }
 
     @EventHandler
-    fun onInteract(event: PlayerInteractEvent) { execute(event.item ?: return, event, PType.INTERACT) }
+    fun onAttack(event: EntityDamageByEntityEvent) = execute(event.entity as? Player, event, PType.ATTACKING)
 
     @EventHandler
-    fun onBowShoot(event: EntityShootBowEvent) { execute(event.bow ?: return, event, PType.SHOOT_BOW) }
+    fun onDefend(event: EntityDamageByEntityEvent) = execute(event.entity as? Player, event, PType.DEFENDING)
+
+    @EventHandler
+    fun onDamage(event: EntityDamageEvent) = execute(event.entity as? Player, event, PType.DAMAGE )
+
+    @EventHandler
+    fun onBlockBreak(event: BlockBreakEvent) = execute(event.player, event, PType.BLOCK_BREAK)
+
+    @EventHandler
+    fun onBlockDamage(event: BlockDamageEvent) = execute(event.player, event, PType.MINING)
+
+    @EventHandler
+    fun onInteract(event: PlayerInteractEvent) = execute(event.player, event, PType.INTERACT)
+
+    @EventHandler
+    fun onBowShoot(event: EntityShootBowEvent) = execute(event.entity as? Player, event, PType.SHOOT_BOW)
 
 }
