4bd236c1248694dbe6c629bc9cedbd7e1ad8c363
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-06-23 19:29:12 -0500

    Fix Passive Task Target Slot

--- plugin/src/main/kotlin/us/teaminceptus/plasmaenchants/PlasmaEnchants.kt
+++ plugin/src/main/kotlin/us/teaminceptus/plasmaenchants/PlasmaEnchants.kt
@@ -7,9 +7,11 @@ import org.bstats.bukkit.Metrics
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.ChatColor
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.HandlerList
 import org.bukkit.event.player.PlayerEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.scheduler.BukkitRunnable
 import us.teaminceptus.plasmaenchants.api.*
 import us.teaminceptus.plasmaenchants.api.artifacts.PArtifact
@@ -31,23 +33,29 @@ private const val github = "Team-Inceptus/PlasmaEnchants"
 class PlasmaEnchants : JavaPlugin(), PlasmaConfig, PlasmaRegistry {
 
     companion object {
+        private fun ItemStack.isArmor(): Boolean {
+            return type.name.endsWith("_HELMET") || type.name.endsWith("_CHESTPLATE") || type.name.endsWith("_LEGGINGS") || type.name.endsWith("_BOOTS")
+        }
+
         @JvmStatic
         internal val passiveTask = object : BukkitRunnable() {
             override fun run() {
-                Bukkit.getOnlinePlayers().forEach {
+                Bukkit.getOnlinePlayers().forEach { p ->
                     val items = listOfNotNull(
-                        it.inventory.armorContents.toList(),
-                        listOf(it.inventory.itemInMainHand),
-                        listOf(it.inventory.itemInOffHand)
+                        p.inventory.armorContents.toList().filter { it != null && it.isArmor() },
+                        listOf(p.inventory.itemInMainHand.takeUnless { it.isArmor() }),
+                        listOf(p.inventory.itemInOffHand.takeUnless { it.isArmor() })
                     ).flatten().filterNotNull()
 
                     if (items.isEmpty()) return@forEach
 
                     for (item in items) {
+                        if (item.type == Material.ENCHANTED_BOOK) continue
                         val meta = item.itemMeta ?: continue
+                        if (meta.isArtifactItem) continue
 
-                        meta.plasmaEnchants.filter { entry -> entry.key.type == PType.PASSIVE }.forEach { entry -> entry.key.accept(PlayerTickEvent(it), entry.value) }
-                        if (meta.hasArtifact() && meta.artifact!!.type == PType.PASSIVE) meta.artifact!!.accept(PlayerTickEvent(it))
+                        meta.plasmaEnchants.filter { entry -> entry.key.type == PType.PASSIVE }.forEach { entry -> entry.key.accept(PlayerTickEvent(p), entry.value) }
+                        if (meta.hasArtifact() && meta.artifact!!.type == PType.PASSIVE) meta.artifact!!.accept(PlayerTickEvent(p))
                     }
                 }
             }
